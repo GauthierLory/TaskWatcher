05473c2a57f97183c3d3baf23db40fcdb5528e45
GauthierLory <gauthier.lory@protonmail.com>
2022-08-12 19:08:29 +0200

    404

--- src/main.js
+++ src/main.js
@@ -1,51 +1,63 @@
-import { createApp } from 'vue'
-import * as VueRouter from 'vue-router'
+import { createApp } from "vue";
+import * as VueRouter from "vue-router";
 
-import App from './App.vue'
-import HomePage from './pages/Home.vue'
-import SettingsPage from './pages/Settings.vue'
-import SettingsApp from './components/SettingsApp.vue'
-import SettingsUser from './components/SettingsUser.vue'
+import App from "./App.vue";
+import HomePage from "./pages/Home.vue";
+import SettingsPage from "./pages/Settings.vue";
+import NotFoundPage from "./pages/NotFound.vue";
+import SettingsApp from "./components/SettingsApp.vue";
+import SettingsUser from "./components/SettingsUser.vue";
 
-import ElementPlus from 'element-plus'
-import 'element-plus/dist/index.css'
+import ElementPlus from "element-plus";
+import "element-plus/dist/index.css";
 
 const router = VueRouter.createRouter({
-    history: VueRouter.createWebHistory(),
-    routes: [
+  history: VueRouter.createWebHistory(),
+  routes: [
+    {
+      path: "/",
+      alias: "/home",
+      name: "Home",
+      component: HomePage,
+      //   children: [
+      //     {
+      //       path: ":taskID",
+      //       component: HomePage,
+      //     },
+      //   ],
+    },
+    {
+      path: "/settings",
+      name: "Settings",
+      component: SettingsPage,
+      children: [
         {
-            path: '/',
-            name: 'Home',
-            component: HomePage,
-            children: [
-                {
-                    name: 'Home',
-                    path: ':taskID',
-                    component: HomePage
-                }
-            ]
+          path: "user",
+          component: SettingsUser,
         },
         {
-            path: '/settings',
-            name: 'Settings',
-            component: SettingsPage,
-            children: [
-                {
-                    path: 'user',
-                     component: SettingsUser
-                },
-                {
-                    path: 'app',
-                    component: SettingsApp
-                }
-            ]
-        }
-    ]
-})
+          path: "app",
+          component: SettingsApp,
+        },
+      ],
+    },
+    {
+      path: "/notfound",
+      name: "NotFound",
+      component: NotFoundPage,
+    },
+    {
+      path: "/:wrongPath(.*)",
+      redirect: (to) => {
+        return { name: "NotFound", params: { wrongPath: to.params.wrongPath } };
+      },
+    },
+  ],
+});
 
-const app = createApp(App)
+const app = createApp(App);
 
-app.use(router)
-app.use(ElementPlus)
+app.use(router);
+app.use(ElementPlus);
 
-app.mount('#app')
+app.mount("#app");
---
+++ src/pages/NotFound.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <h1>404</h1>
+    <p>{{ $route.params.wrongPath }}</p>
+    <el-button @click="$router.go(-1)" type="primary" round>
+      Go back
+    </el-button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "VueTrackerNotFound",
+
+  data() {
+    return {};
+  },
+
+  mounted() {},
+
+  methods: {},
+};
+</script>
+
+<style lang="scss" scoped></style>
